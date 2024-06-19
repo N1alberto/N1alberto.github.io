976f3193794f375442d6edb2e0ba1df79937b51d
N1alberto <112620933+N1alberto@users.noreply.github.com>
2024-06-19 15:01:33 +0200

    Games are shown on the page

---
+++ FerretStudio/js/main.js
@@ -0,0 +1,22 @@
+const games = document.querySelector('.dump');
+
+fetch("/FerretStudio/Data/Projects.JSON")
+.then(myData => myData.json())
+.then(gameData => showOnPage(gameData));
+
+function showOnPage(data) {
+    for (let i = 0; i < data.Projects.length; i++) {
+        const projects = data.Projects[i];
+        console.log(projects)
+        games.innerHTML += `<div class="col-md-4">
+        <div class="card">
+    <img class="card-img-top" src="`+ projects.img +`" alt="Title" />
+    <div class="card-body">
+        <h4 class="card-title">`+ projects.title +`</h4>
+        <p class="card-text"> `+ projects.description +`</p>
+    </div>
+</div></div>
+        `
+        
+    }
+}
